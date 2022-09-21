8d1a7c91b94e6a9ebd8545d3bb75cab12382220e
Kutman Amangeldiev <kutmanamangeldiev@Kutmans-MacBook-Pro.local>
2022-09-21 13:38:29 +0800

    timer countdown

--- FirstAplication/ViewController.swift
+++ FirstAplication/ViewController.swift
@@ -12,19 +12,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var eggLabel: UILabel!
     
     let eggs = ["Soft": 5, "Medium": 7, "Hard": 12]
-    
+    var secondsRemaining = 60
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func hardnessPressed(_ sender: UIButton) {
-        if sender.currentTitle != nil{
-            print(sender.currentTitle!)
-        } else {
-            print("ther is not title for this button")
-        }
+        let hardness = sender.currentTitle!
+        eggLabel.text = hardness
+        timer.invalidate()
+        secondsRemaining = eggs[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
     
+    @objc func update(){
+        if secondsRemaining > 0{
+            print("\(secondsRemaining) seconds")
+            secondsRemaining = secondsRemaining - 1
+        } else{
+            timer.invalidate()
+            eggLabel.text = "Done"
+        }
+    }
 }
 
